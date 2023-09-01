03eadf0d243f827902f413f65ff76327b7b9f87f
Chinmay Giri <96367554+chinmaygiri@users.noreply.github.com>
2023-09-01 16:14:30 +0530

    Create Day 8 - Beginner - Function Parameters & Caesar Cipher.py

---
+++ Day8/Day 8 - Beginner - Function Parameters & Caesar Cipher.py
@@ -0,0 +1,8 @@
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+
+greet_with("Jack Bauer", "Nowhere")
+greet_with(location="Nowhere", name="Jack Bauer")
+
